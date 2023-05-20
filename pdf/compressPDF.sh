9ebc705252073ff63376bdbe02970714f8ea225d
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-05-20 12:18:55 +0200

    update before laptopn breaking down!

--- pdf/compressPDF.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# compress pdf
-
-echo Compressing pdf figure...
-
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dBATCH -sOutputFile=out.pdf in.pdf
-
-echo Done... Written output.pdf!
-
